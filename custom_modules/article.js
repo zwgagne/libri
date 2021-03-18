66f4e940a669f26b7c898383de4ae794a3e79c88
Zacharie-William Gagné <zwgagne@outlook.com>
2021-03-18 18:55:14 -0400

    created the custom_modules folder and added the article module and the class

---
+++ custom_modules/article.js
@@ -0,0 +1,9 @@
+class article{
+    constructor(id, author, date, subject, content){
+        this.id = id;
+        this.author = author;
+        this.date = date;
+        this.subject = subject;
+        this.content = content;
+    }
+}
